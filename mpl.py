e7bdc53fcdd358af78988165c1c0fa884c7bfd30
zpywoyaoainiyibeizi <1010167895@qq.com>
2018-09-18 16:19:43 +0800

    mpl
    优化程序

--- mpl.py
+++ mpl.py
@@ -1,24 +1,27 @@
-
+import os
 name_list=[]
 res_list=[]
-chaifen_list=[]
+path_name='name_number_price'
 def file_read():
-    with open('name.txt','r',encoding='utf-8') as f:
-        for line in f:
-            real_line=line.strip()
-            name_list.append(real_line)
+    with open(path_name+'.txt','r',encoding='utf-8') as De_weighting:
+        for line in De_weighting:
+            real_line = line.strip()
+            row = real_line.split('\t')
+            index0 = row[0]
+            if index0 not in name_list:
+                name_list.append(index0)
 
-        for name in name_list:
-            with open('name_number_price.txt', 'r', encoding='utf-8') as file:
-                with open(name + '.txt', 'w', encoding='utf-8') as write_file:
-                    for line in file:
-                        real_line = line.strip()
-                        row = real_line.split('\t')
-                        index0 = row[0]
-                        if index0 == name:
-                            write_file.write(line)
-                        else:
-                            continue
+    for name in name_list:
+        with open(path_name+'.txt', 'r', encoding='utf-8') as file:
+            with open(name + '.txt', 'w', encoding='utf-8') as write_file:
+                for line in file:
+                    real_line = line.strip()
+                    row = real_line.split('\t')
+                    index0 = row[0]
+                    if index0 == name:
+                        write_file.write(line)
+                    else:
+                        continue
     shuliang_float = 0
     zongjia_float = 0
     for name in name_list:
@@ -27,18 +30,25 @@ def file_read():
                real_line = line.strip()
                row = real_line.split('\t')
                index0 = row[0]
-               index1 = float(row[1])
+               index1 = float (row[1])
                index2 = float (row[2])
                index3 = float (row[3])
+
                shuliang_float=shuliang_float+index1
                zongjia_float=zongjia_float+index3
         junjia=zongjia_float/shuliang_float
-        write_line=name+'\t'+str(shuliang_float)+'\t'+str(junjia)+'\t'+str(zongjia_float)
+        Quantity=float('%.2f' % shuliang_float)
+        Total_price=float('%.2f' % zongjia_float)
+        Average_price=float('%.2f' % junjia)
+        write_line=name+'\t'+str(Quantity)+'\t'+str(Average_price)+'\t'+str(Total_price)
         res_list.append(write_line)
         shuliang_float = 0
         zongjia_float = 0
-    with open('res.txt','w',encoding='utf-8') as write_file:
+        os.remove(name+'.txt')
+
+    with open(path_name+'_res.txt','w',encoding='utf-8') as write_file:
         for line in res_list:
 
             write_file.write(line+'\n')
+
 file_read()
